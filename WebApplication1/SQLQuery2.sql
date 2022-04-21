f5fde13a5c24f677c15ab87ab939edacff0e294e
denkaforss <dennis.forss@gmail.com>
2022-04-21 09:15:38 +0200

    changes

---
+++ WebApplication1/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿SELECT * FROM dbo.SuperHeroes;
+
+INSERT INTO dbo.SuperHeroes(Name, FirstName, LastName, Place)
+VALUES ('Batman', 'Bruce', 'Wayne', 'Gotham City'),
+('Iron Man', 'Tony', 'Stark'
+
+DELETE FROM dbo.SuperHeroes WHERE Id = 3;
+
